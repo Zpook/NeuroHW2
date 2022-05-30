03beb890b301babf10f4d044791cf76cc3651cdc
Mikel <mikelk96@gmail.com>
2022-05-30 17:20:37 +0300

    changes

--- Question B.py
+++ Question B.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import torch
 
 from som.mapping import SOM
 
@@ -8,33 +9,36 @@ x = np.linspace(0, 1, 100)
 y = np.linspace(0, 1, 100)
 XX, YY = np.meshgrid(x, y)
 
-data = np.vstack([ XX.reshape(-1), YY.reshape(-1) ]).transpose()
+data = np.vstack([XX.reshape(-1), YY.reshape(-1)]).transpose()
 
-parameters = {'n_points'  : 500,
-              'alpha0'    : 0.5,
-              't_alpha'   : 25,
-              'sigma0'    : 2,
-              't_sigma'   : 25,
-              'epochs'    : 300,
-              'seed'      : 124,
-              'scale'     : True,
-              'shuffle'   : True,
-              'history'   : True}
+x = np.linspace(0, 1, 15)
+y = np.linspace(0, 1, 15)
+XX, YY = np.meshgrid(x, y)
 
-# Load and train the model
-model = SOM()
-model.set_params(parameters)
-model.fit(data)
+weights = np.vstack([XX.reshape(-1), YY.reshape(-1)]).transpose()
+weights = torch.tensor(weights)
+
+model = SOM(
+    alpha0=0.5,
+    t_alpha=25,
+    sigma0=2,
+    t_sigma=25,
+    attribNum=2,
+    weights=weights,
+    scale=True,
+    history=True,
+)
+model.fit(data, 300)
 
 weights = model.get_weights()
 
 # Plot the train dataset and the weights
 historyNum = model.history.shape[0]
 for historyIndex in range(historyNum):
-    weights = model.history[historyIndex,:,:]
+    weights = model.history[historyIndex, :, :]
     fig, ax = plt.subplots()
     fig.suptitle("Train set (PCA-reduced) and weights")
-    t = ax.scatter(data[:,0], data[:,1])
+    t = ax.scatter(data[:, 0], data[:, 1])
     w = ax.scatter(weights[:, 0], weights[:, 1])
     fig.legend((t, w), ("Train", "Weights"))
     plt.show()
--- som/mapping.py
+++ som/mapping.py
@@ -2,94 +2,95 @@ import torch
 from math import exp
 from tqdm import trange
 
-class SOM:
-    def __init__(self, n_points=500, alpha0=0.5, t_alpha=25,
-                 sigma0=2, t_sigma=25, epochs=300, seed=42,
-                 shuffle=True, scale=True, history=False):
-        
-        self.params = {'n_points' : n_points, # The number of points/weights
-                       'alpha0'   : alpha0,   # Initial learning rate
-                       't_alpha'  : t_alpha,  # Exponential reduction constant (alpha)
-                       'sigma0'   : sigma0,   # Initial neighborhood strenght 
-                       't_sigma'  : t_sigma,  # Exponential reduction constant (sigma)
-                       'epochs'   : epochs,   # Number of iteration
-                       'seed'     : seed,     # Random seed
-                       'scale'    : scale,    # Make max(W) = max(X) and min(W) = min(X)
-                       'history'  : history,  # Save all the Ws (debugging)
-                       'shuffle'  : shuffle}  # Shuffle the train set
-        
-        if self.params['seed']:
-            torch.manual_seed(seed)
-    
-    def fit(self, X):
-        n_samples, n_attributes = X.shape
 
-        # Initialize the points randomly (weights)
-        self.W = torch.rand((self.params['n_points'], n_attributes),
-                             dtype=torch.double)
+class SOM:
+    def __init__(
+        self,
+        alpha0=0.5,
+        t_alpha=25,
+        sigma0=2,
+        t_sigma=25,
+        attribNum=2,
+        weights=None,
+        scale=True,
+        history=False,
+    ):
+
+        self.alpha = alpha0
+        self.t_alpha = t_alpha
+        self.sigma = sigma0
+        self.t_sigma = t_sigma
+        self.allowScale = scale
+        self.allowHistory = history
+
+        self.weights = weights
+        self.n_points = weights.shape[0]
+        self.attributeNumber = weights.shape[1]
+
+    def fit(self, X, epochs):
+        n_samples, n_attributes = X.shape
 
         # From numpy conversion
         X = torch.from_numpy(X).type(torch.double)
-        
-        # Shuffling
-        if self.params['shuffle']:
-            indices = torch.randperm(n_samples)
-            X = X[indices, :]
 
         # Scaling W in the same range as X
-        if self.params['scale']:
-            self.W = self.W*(torch.max(X) - torch.min(X)) + torch.min(X)
+        if self.allowScale:
+            self.weights = self.weights * (torch.max(X) - torch.min(X)) + torch.min(X)
 
         # Record each W for each t (debugging)
-        if self.params['history']:
-            self.history = self.W.reshape(1, self.params['n_points'], n_attributes)      
+        if self.allowHistory:
+            self.history = self.weights.reshape(1, self.n_points, self.attributeNumber)
 
         # The training loop
-        for t in trange(self.params['epochs']):
-            x       = X[t % n_samples, :] # The current sampled x
-            x_dists = x - self.W          # Distances from x to W
-                    
+        for t in trange(epochs):
+            x = X[t % n_samples, :]  # The current sampled x
+            x_dists = x - self.weights  # Distances from x to W
+
             # Find the winning point
-            distances = torch.pow((x_dists), 2).sum(axis=1) # [n_points x 1]
-            winner    = torch.argmin(distances)
+            distances = torch.pow((x_dists), 2).sum(axis=1)  # [n_points x 1]
+            minDistance = torch.argmin(distances)
 
             # Lateral distance between neurons
-            lat_dist = torch.pow((self.W - self.W[winner, :]), 2).sum(axis=1) # [n_points x 1]
+            lateralDistance = torch.pow((self.weights - self.weights[minDistance, :]), 2).sum(
+                axis=1
+            )  # [n_points x 1]
 
             # Update the learning rate
-            alpha = self.params['alpha0']*exp(-t/self.params['t_alpha']) # [scalar]
+            alpha = self.alpha * exp(-t / self.t_alpha)  # [scalar]
 
             # Update the neighborhood size
-            sigma = self.params['sigma0']*exp(-t/self.params['t_sigma']) # [scalar]
+            sigma = self.sigma * exp(-t / self.t_sigma)  # [scalar]
 
             # Evaluate the topological neighborhood
-            h = torch.exp(-lat_dist/(2*sigma**2)).reshape((self.params['n_points'], 1)) # [n_points x 1]
+            h = torch.exp(-lateralDistance / (2 * sigma**2)).reshape(
+                (self.n_points, 1)
+            )  # [n_points x 1]
 
             # Update W
-            self.W += alpha*h*(x_dists)
+            self.weights += alpha * h * (x_dists)
 
-            if self.params['history']:
-                self.history = torch.cat((self.history,
-                                          self.W.reshape(1, self.params['n_points'], n_attributes)),
-                                         axis=0)
+            if self.allowHistory:
+                self.history = torch.cat(
+                    (
+                        self.history,
+                        self.weights.reshape(1, self.n_points, self.attributeNumber),
+                    ),
+                    axis=0,
+                )
 
     def adjacency_matrix(self, M):
         M = torch.from_numpy(M)
         n_samples, n_attributes = M.shape
-        
+
         # Broadcast the M matrix to a tensor of the shape
         # (n_sample, (n_samples, n_attributes))
 
-        tensor    = M.repeat(n_samples, 1, 1)              # Make n_samples copy of M
-        M_flat    = M.reshape(n_samples, 1, n_attributes)  # Each row of M or each copy in tensor
+        tensor = M.repeat(n_samples, 1, 1)  # Make n_samples copy of M
+        M_flat = M.reshape(
+            n_samples, 1, n_attributes
+        )  # Each row of M or each copy in tensor
         distances = torch.pow((tensor - M_flat), 2).sum(axis=2).sqrt()
         return distances
-        
-    def set_params(self, params):
-        self.params = {param: value for param, value in params.items()}
 
-    def get_params(self):
-        return self.params
-  
     def get_weights(self):
-        return self.W
+        return self.weights
