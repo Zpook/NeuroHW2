448c502306f8b4e405d1ccbfe1c86bc34b0440a9
Mikel <mikelk96@gmail.com>
2022-06-02 21:20:16 +0300

    working stuff

--- Question B2.py
+++ Question B2.py
@@ -7,14 +7,14 @@ from ShapeGen import Rectangle, ShapeGen
 from som.mapping import SOM
 
 
-SHOW_HISTORY = False
+SHOW_HISTORY = True
 HISTORY_STEPSIZE = 500
 SHOW_OUTPUT = True
 SHOW_INPUT = False
 
 USE_EXAMPLE_DATA = False
 
-EPOCHS = 5000
+EPOCHS = 2000
 ALPHA_SCALE = 1
 SIGMA_SCALE = 1
 ALPHA = 0.2
@@ -27,14 +27,13 @@ XX, YY = np.meshgrid(x, y)
 
 data = np.vstack([XX.reshape(-1), YY.reshape(-1)]).transpose()
 
-
 palmShape = Rectangle((0.15, 0.15), (0.85, 0.35))
 finger1 = Rectangle((0.15, 0.25), (0.25, 0.65))
 finger2 = Rectangle((0.35, 0.35), (0.45, 0.75))
 finger3 = Rectangle((0.55, 0.35), (0.65, 0.85))
 finger4 = Rectangle((0.75, 0.35), (0.85, 0.75))
 
-allShapes = [palmShape, finger1, finger2, finger3, finger4]
+allShapes = [palmShape, finger1, finger3, finger4]
 
 shapeFilter = ShapeGen(allShapes)
 
@@ -44,8 +43,7 @@ x = np.linspace(0, 1, 15)
 y = np.linspace(0, 1, 15)
 XX, YY = np.meshgrid(x, y)
 
-weights = np.vstack([XX.reshape(-1), YY.reshape(-1)]).transpose()
-weights = torch.tensor(weights)
+weights = torch.load("model.pt")
 
 
 if USE_EXAMPLE_DATA:
@@ -120,5 +118,3 @@ if SHOW_HISTORY:
         plt.xlim((0, 1))
         plt.ylim((0, 1))
         plt.show()
-
-torch.save(model.weights,"./model.pt")
